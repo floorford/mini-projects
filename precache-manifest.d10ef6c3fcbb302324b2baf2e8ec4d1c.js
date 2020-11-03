9b5ad371e1e443b281bc26ad55b66d5e4a91e73c
Flo Orford <flo.orford@hotmail.com>
2020-11-03 11:16:59 +0000

    Updates

---
+++ precache-manifest.d10ef6c3fcbb302324b2baf2e8ec4d1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7168473a93067be45ac8e0e7b2238b4",
+    "url": "/mini-projects/index.html"
+  },
+  {
+    "revision": "80f6afad4e64e7001a60",
+    "url": "/mini-projects/static/css/main.5cd3796f.chunk.css"
+  },
+  {
+    "revision": "679eba6145760fd2df07",
+    "url": "/mini-projects/static/js/2.e4420b23.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mini-projects/static/js/2.e4420b23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80f6afad4e64e7001a60",
+    "url": "/mini-projects/static/js/main.7174e60d.chunk.js"
+  },
+  {
+    "revision": "8c585e07bf29fdb985de",
+    "url": "/mini-projects/static/js/runtime-main.7b5793a7.js"
+  }
+]);
